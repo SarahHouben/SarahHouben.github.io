7e4f1280c43ad576ba297a2ef83aa33605991621
SarahHouben <sarahhouben@outlook.com>
2019-09-27 23:22:33 +0200

    faded out h1 on main page

--- main.js
+++ main.js
@@ -2,26 +2,23 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
   let boxes = document.querySelectorAll(".box");
-  //   let boxHeaders = document.querySelectorAll(".box-header");
   boxes.forEach(box => {
     box.addEventListener("click", function() {
-      // if( box.classList.contains('box--active')) {
-      // 	 box.classList.remove('box--active');
-      // 	 if( boxInner ) boxInner.classList.remove('box__inner--enter');
-      // }
-      // else {
-      // 	 box.classList.add('box--active');
-      // 	 if( boxInner ) boxInner.classList.add('box__inner--enter');
-      // }
       function removeActiveClasses(box) {
         box.classList.remove("box--active");
+
+        let boxHeader = box.querySelector(".box__header");
+        boxHeader.classList.remove("box-header--active");
+
         let boxInner = box.querySelector(".box__inner");
         if (boxInner) boxInner.classList.remove("box__inner--enter");
       }
 
       function addActiveClasses(box) {
         box.classList.add("box--active");
-        // boxHeaders.classList.add("box-header--active");
+
+        let boxHeader = box.querySelector(".box__header");
+        boxHeader.classList.add("box-header--active");
 
         let boxInner = box.querySelector(".box__inner");
         if (boxInner) boxInner.classList.add("box__inner--enter");
@@ -41,11 +38,5 @@ function ready() {
         addActiveClasses(box);
       }
     });
-
-    // box.addEventListener('click', function(){
-    // 				box.classList.remove('box--active');
-
-    // 	if( boxInner ) boxInner.classList.remove('box__inner--enter');
-    // })
   });
 }
